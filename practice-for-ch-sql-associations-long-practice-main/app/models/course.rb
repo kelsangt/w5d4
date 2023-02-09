543eab7ca376a27fd9cbf6995a79ef3cc47cdad6
“Kelsang” <kelsangrtsering@gmail.com>
2023-02-09 15:08:20 -0500

    Start work on Associations Exercise

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+    # has_many(
+    #     :students,
+    #     class_name: 'Student',
+    #     foreign_key: :id
+    # )
 end
--- practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/user.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+    belongs_to(
+        :enrolled_courses,
+        class_name: :Course,
+        foreign_key: :id
+    )
+    has_many(
+        :enrollments,
+        class_name: :Enrollment,
+        foreign_key: :id
+    )
 end
